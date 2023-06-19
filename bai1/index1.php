c78b2a5ea30b1893d88cb5e66744928b1e52cded
Trần Hải <tranthanhhai200000@gmail.com>
2023-06-19 09:10:36 +0700

    BaitapPhp2

---
+++ bai1/index1.php
@@ -0,0 +1,16 @@
+<?php
+$phicodinh = 25000;
+$sophut = readline('Nhập số phút: ');
+
+if ($sophut > 200) {
+    echo "Phí phải trả: " . (($sophut - 200) * 200 + 150 * 400 + 50 * 600);
+} else if ($sophut > 50 && $sophut < 200) {
+    echo $sophut * 400 + 50 * 600;
+} else if ($sophut < 50) {
+    echo $sophut * 600;
+} else {
+    echo $phicodinh;
+}
+?>
+
+
---
+++ bai2/index2.php
@@ -0,0 +1,11 @@
+
+<?php
+
+
+for($AB = 1 ; $AB <=50 ; $AB++) {
+    if ($AB % 2 == 0) {
+        echo "<div style='color:red'>" . $AB . "</div></br>";
+    } else {
+        echo "<div style='color:blue'>" . $AB . "</div></br>";
+    }
+}
